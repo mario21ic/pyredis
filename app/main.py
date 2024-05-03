f6acd1401c67fa8195a35e31c243e135ccc9e276
Mario IC <mario21ic@gmail.com>
2024-05-02 19:11:44 -0500

    respond to multiple pings

--- app/main.py
+++ app/main.py
@@ -9,11 +9,15 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-    data = conn.recv(1024).decode() # read data from client
-    print(data)
-    if data == PING:
-        conn.sendall(f"{RESPONSE}".encode()) # send data to client
-        print("Response sent")
+    with conn:
+        while True:
+            data = conn.recv(1024).decode() # read data from client
+            if not data:
+                break
+            print(data)
+            if data == PING:
+                conn.sendall(f"{RESPONSE}".encode()) # send data to client
+                print("Response sent")
 
 
 if __name__ == "__main__":

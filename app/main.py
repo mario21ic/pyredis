6b2f1b27e9c840da4d14b62e09149385ab27ffdb
Mario IC <mario21ic@gmail.com>
2024-05-03 01:33:59 -0500

    Send handshake (2/3)

--- app/main.py
+++ app/main.py
@@ -17,6 +17,21 @@ def create_bulk_response(msg):
     print("bulk_resp", bulk_resp)
     return bulk_resp
 
+"""
+# REPLCONF listening-port <PORT>
+*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n
+# REPLCONF capa psync2
+*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n
+https://redis.io/docs/latest/develop/reference/protocol-spec/#arrays
+"""
+def bulk_array(text):
+    message = text.split(" ")
+    bulk = "*" + str(len(message)) + "\r\n"
+    for msg in message:
+        bulk += "$" + str(len(msg)) + "\r\n" + msg + "\r\n"
+    print("bulk", bulk)
+    return bulk
+
 async def count_down(key: str, px: int = None):
     def delete_key():
         print("delete_key - time expired:", key)
@@ -32,6 +47,7 @@ async def handle_client(client: socket.socket):
     while req := await loop.sock_recv(client, 1024):
         print(f"received request: {req} - client: {client}")
         cmd = req.decode().split("\r\n")[2].lower()
+        print(f"cmd: {cmd}")
         match cmd:
             case "ping":
                 await loop.sock_sendall(client, PONG.encode())
@@ -62,6 +78,10 @@ async def handle_client(client: socket.socket):
                 param = req.decode().split("\r\n")[4]
                 if param == "replication":
                     await loop.sock_sendall(client, create_bulk_response(f"# Replication\r\nrole:{ROLE}\r\nmaster_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb\r\nmaster_repl_offset:0\r\n").encode())
+            case "replconf":
+                await loop.sock_sendall(client, OK.encode())
+            case _:
+                await loop.sock_sendall(client, create_bulk_response("Command not found").encode())
 
 
 async def main():
@@ -71,10 +91,10 @@ async def main():
     parser.add_argument("--replicaof", nargs=2, default="", metavar=("MASTER_HOST", "MASTER_PORT"))
     args = parser.parse_args(sys.argv[1:])
 
-    port = 6379
+    host_port = 6379
     if args.port:
         # print(f"port turned on: {args.port}")
-        port = args.port
+        host_port = args.port
     
     # Mode Replica
     if len(args.replicaof) > 0:
@@ -85,12 +105,30 @@ async def main():
         # Send Ping to Master
         master_sock = socket.create_connection((master_host, int(master_port)))
         master_sock.sendall(PING.encode())
+        
+        # Recibir respuesta del servidor
+        response = master_sock.recv(1024)  # 1024 es el tamaño del buffer de recepción
+        print("response", response.decode())
+        if response.decode() == PONG:
+            print("Master is alive")
+            
+            replconf1 = f"REPLCONF listening-port {host_port}"
+            master_sock.sendall(bulk_array(replconf1).encode())
+            response = master_sock.recv(1024)
+            print("response", response.decode())
+
+            replconf2 = "REPLCONF capa psync2"
+            master_sock.sendall(bulk_array(replconf2).encode())
+            response = master_sock.recv(1024)
+            print("response", response.decode())
+        
+        # Close connection
         master_sock.close()
 
-    server = socket.create_server(("localhost", port), reuse_port=False)
+    server = socket.create_server(("localhost", host_port), reuse_port=False)
     server.setblocking(False)
     server.listen()
-    print(f"Listening on port {port}")
+    print(f"Listening on port {host_port}")
     loop = asyncio.get_event_loop()
     while True:
         client, _ = await loop.sock_accept(server)

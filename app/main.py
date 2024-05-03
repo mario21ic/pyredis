3265fe57d00a68db42ec1ca5d0c6753dd29c1c1b
Mario IC <mario21ic@gmail.com>
2024-05-02 21:45:22 -0500

    implement the echo command

--- app/main.py
+++ app/main.py
@@ -1,18 +1,32 @@
 import socket
 import asyncio
 
+
 # PING = "*1\r\n$4\r\nping\r\n"
 PONG = "+PONG\r\n"
+
+# https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
+def create_bulk_response(msg):
+    bulk_resp = "$" + str(len(msg)) + "\r\n" + msg + "\r\n"
+    print("bulk_resp", bulk_resp)
+    return bulk_resp
+
 
 async def handle_client(client: socket.socket):
     loop = asyncio.get_event_loop()
     while req := await loop.sock_recv(client, 1024):
-        print("receibed request", req, client)
-        await loop.sock_sendall(client, PONG.encode())
+        print(f"received request: {req} - client: {client}")
+        match req.decode().split("\r\n")[2].lower():
+            case "ping":
+                await loop.sock_sendall(client, PONG.encode())
+            case "echo":
+                msg = req.decode().split("\r\n")[4]
+                await loop.sock_sendall(client, create_bulk_response(msg).encode())
+
 
 async def main():
     print("Starting server...")
-    server = socket.create_server(("localhost", 6379), reuse_port=True)
+    server = socket.create_server(("localhost", 6379), reuse_port=False)
     server.setblocking(False)
     server.listen()
     print("Listening on port 6379")

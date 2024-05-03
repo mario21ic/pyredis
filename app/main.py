c808b5b81c144af0cc8df7b131222debd54e4182
Mario IC <mario21ic@gmail.com>
2024-05-02 21:27:48 -0500

    handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,24 +1,28 @@
 import socket
+import asyncio
 
-PING = "*1\r\n$4\r\nping\r\n"
+# PING = "*1\r\n$4\r\nping\r\n"
 RESPONSE = "+PONG\r\n"
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+async def pong(client_socket: socket.socket, loop: asyncio.AbstractEventLoop):
+    while data := await loop.sock_recv(client_socket, 1024):
+        await loop.sock_sendall(client_socket, RESPONSE.encode())
 
+
+async def listen(server_socket: socket.socket, loop: asyncio.AbstractEventLoop):
+    while True:
+        client_socket, addr = await loop.sock_accept(server_socket)
+        client_socket.setblocking(False)
+        asyncio.create_task(pong(client_socket, loop))
+
+
+async def main():
+    print("Starting server...")
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
-    with conn:
-        while True:
-            data = conn.recv(1024).decode() # read data from client
-            if not data:
-                break
-            print(data)
-            # if data == PING:
-            conn.sendall(f"{RESPONSE}".encode()) # send data to client
-            print("Response sent")
+    server_socket.setblocking(False)
+    loop = asyncio.get_event_loop()
+    await listen(server_socket, loop)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
